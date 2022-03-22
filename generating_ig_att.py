ac97c9cefe14a06cdfec125d76b218c087cf3718
Swetank Poddar <2424088p@student.gla.ac.uk>
2022-03-22 19:49:26 +0000

    dev

--- generating_ig_att.py
+++ generating_ig_att.py
@@ -14,7 +14,7 @@ def cal_ig(m, dx, dy):
     if i==0:
         att = calculate_ig(model=m, beats=x_batch, class_indexes=y_batch)
     else:
-        att = np.append(att,calculate_ig(model=m, beats=x_batch, class_indexes=tf.reduce_max(y_batch, axis=1)),axis=0)
+        att = np.append(att,calculate_ig(model=m, beats=x_batch, class_indexes=y_batch),axis=0)
     print(att.shape)
         # att = np.append(att,eg(inputs=x_batch, labels=y_batch, model=m),axis=0)
 #   return att
