060d2def7917aae3423df3f370ca2a6179c75465
Swetank Poddar <2424088p@student.gla.ac.uk>
2022-03-29 08:53:57 +0100

    cnn pl ig

--- cnn_ig_f.py
+++ cnn_ig_f.py
@@ -38,17 +38,16 @@ from tensorflow.keras.metrics import CategoricalAccuracy
 from sklearn.model_selection import train_test_split
 from alibi.explainers import IntegratedGradients
 
-# Check for GPU
+
 print("You are using Tensorflow version", tf.__version__)
 
 print(tf.config.list_physical_devices('GPU'))
 
-print("-"*100)
-
-# Defining the path
 path = './'
 
-# Loading data
+print("-"*100)
+
+# Loading Data
 train_c1 = np.genfromtxt(path+'Data/train_patients_sc.csv', delimiter=',')
 train_c0 = np.genfromtxt(path+'Data/train_patients_fc.csv', delimiter=',')
 test_c1 = np.genfromtxt(path+'Data/test_patients_sc.csv', delimiter=',')
@@ -73,7 +72,6 @@ print("x:", test_x.shape, "y:", test_y.shape)
 
 print("-"*100)
 
-
 def showResults(test, pred, model_name):
     accuracy = accuracy_score(test, pred)
     precision= precision_score(test, pred, average='macro')
@@ -89,12 +87,12 @@ def showResults(test, pred, model_name):
     return (model_name, round(accuracy,3), round(precision,3) , round(recall,3) , round(f1score_macro,3), 
             round(f1score_micro, 3), cm)
 
-
 opt = Adam(learning_rate=0.001)
 
-def getCNNModel():
+
+def getCNNModel(i1, i2):
     
-    input = keras.layers.Input(shape=(train_x.shape[1],train_x.shape[2]))
+    input = keras.layers.Input(shape=(i1,i2))
     x = keras.layers.Conv1D(kernel_size=16, filters=32, strides=1, use_bias=True, kernel_initializer='VarianceScaling')(input)
     x = keras.layers.BatchNormalization()(x)
     x = keras.layers.LeakyReLU(alpha=0.3)(x)
@@ -136,28 +134,6 @@ def getCNNModel():
     model.compile(loss='categorical_crossentropy',optimizer=opt,metrics=['accuracy'])
     return model
 
-cnn1 = getCNNModel()
-# cnn2 = getCNNModel()
-# cnn3 = getCNNModel()
-# cnn4 = getCNNModel()
-# cnn5 = getCNNModel()
-# cnn6 = getCNNModel()
-# cnn7 = getCNNModel()
-# cnn8 = getCNNModel()
-# cnn9 = getCNNModel()
-# cnn10 = getCNNModel()
-
-modellist = [cnn1]#, cnn2, cnn3, cnn4, cnn5, cnn6, cnn7, cnn8, cnn9, cnn10]
-
-lamb = 0.0001
-num_epochs = 10
-batch_size = 50
-optimizer = tf.optimizers.Adam(learning_rate = 0.0001)
-loss_fn = tf.keras.losses.CategoricalCrossentropy()
-val_loss_fn = tf.keras.losses.CategoricalCrossentropy()
-train_acc_fn = tf.keras.metrics.CategoricalAccuracy()
-val_acc_fn = tf.keras.metrics.CategoricalAccuracy()
-test_acc_fn = tf.keras.metrics.CategoricalAccuracy()
 
 #train step
 # @tf.function
@@ -187,8 +163,9 @@ def train_step(model_m, inputs, labels):
     gradients = tape.gradient(total_loss, model_m.trainable_variables)
     optimizer.apply_gradients(zip(gradients, model_m.trainable_variables))
     return predictions,total_loss
+
 # function to train a model
-def training(model):
+def training(model, train_x, train_y):
     training_acc = []
     training_loss = []
     validation_acc = []
@@ -208,15 +185,15 @@ def training(model):
         val_x = train_x[val_indices]
         val_y = train_y[val_indices]
         
-
+        e_loss = np.array([])
         for i in range(0, len(new_train_x), batch_size):
             x_batch_train = new_train_x[new_indices[i:min(i + batch_size, len(new_train_x))]]
             y_batch_train = new_train_y[new_indices[i:min(i + batch_size, len(new_train_y))]]
 
-            predictions,epoch_loss = train_step(model, x_batch_train, y_batch_train)
-
+            predictions,epoch_loss = train_step(model, tf.convert_to_tensor(x_batch_train), tf.convert_to_tensor(y_batch_train))
+            e_loss = np.append(e_loss, epoch_loss.numpy())
             train_acc_fn(y_batch_train, predictions)
-
+            print(i)
         train_acc = train_acc_fn.result().numpy()
 
         # validating
@@ -226,17 +203,40 @@ def training(model):
 
         # appending training loss and acc
         training_acc.append(train_acc)
-        training_loss.append(epoch_loss.numpy())
+        training_loss.append(e_loss.mean())
         # appending validation loss and acc
         validation_acc.append(val_acc_fn.result().numpy())
         validation_loss.append(val_loss)
 
-        print('Epoch {} - train_accuracy: {:.4f}, train_loss: {:.4f} | val_accuracy: {:.4f}, val_loss: {:.4f} ({:.1f} seconds / epoch)'.format(epoch + 1, train_acc, epoch_loss, val_acc_fn.result().numpy(), val_loss, time.time()-start_time))
+        print('Epoch {} - train_accuracy: {:.4f}, train_loss: {:.4f} | val_accuracy: {:.4f}, val_loss: {:.4f} ({:.1f} seconds / epoch)'.format(epoch + 1, train_acc, e_loss.mean(), val_acc_fn.result().numpy(), val_loss, time.time()-start_time))
 
         start_time = time.time()
         train_acc_fn.reset_states()
         val_acc_fn.reset_states()
     return training_loss, validation_loss, training_acc, validation_acc
+cnn1 = getCNNModel(train_x.shape[1],train_y.shape[2])
+# cnn2 = getCNNModel()
+# cnn3 = getCNNModel()
+# cnn4 = getCNNModel()
+# cnn5 = getCNNModel()
+# cnn6 = getCNNModel()
+# cnn7 = getCNNModel()
+# cnn8 = getCNNModel()
+# cnn9 = getCNNModel()
+# cnn10 = getCNNModel()
+
+modellist = [cnn1]#, cnn2, cnn3, cnn4, cnn5, cnn6, cnn7, cnn8, cnn9, cnn10]
+
+lamb = 0.0001
+num_epochs = 10
+batch_size = 50
+optimizer = tf.optimizers.Adam(learning_rate = 0.0001)
+loss_fn = tf.keras.losses.CategoricalCrossentropy()
+val_loss_fn = tf.keras.losses.CategoricalCrossentropy()
+train_acc_fn = tf.keras.metrics.CategoricalAccuracy()
+val_acc_fn = tf.keras.metrics.CategoricalAccuracy()
+test_acc_fn = tf.keras.metrics.CategoricalAccuracy()
+
 
 modelosses = []
 modeleval_losses = []
@@ -285,6 +285,8 @@ np.save(new_path+"cnn_cms.npy", cms)
 np.save(new_path+"cnn_history.npy", history)
 
 # Saving the models
+sm_path = "./saved_models/cnn_pl_eg/"
 for m in range(len(modellist)):
     print("*"*10,"Model", m+1, "*"*10)
     modellist[m].save(new_path+"Model"+str(m)+".h5")
+
